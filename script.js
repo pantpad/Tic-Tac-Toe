a1cbf8e590cadaf6120885929fadd5a97ed71002
pantpad <138708449+pantpad@users.noreply.github.com>
2023-09-13 11:12:04 +0200

    Add new reset button

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 console.log('y');
 
+const Player = (name,input) => {
+    const selectionArray = ['', '', '', '', '', '', '', '', ''];
+    return {name,input,selectionArray};
+}
+
+const gameController = (function(){
+    //Object Properties
+    const user = Player('user','X');
+    const cpu = Player('cpu','Y');
+
+    function addMarkToPlayer(input,index){
+        if(input == 'X'){
+            user.selectionArray[index] = input;
+        }
+        if(input == 'O'){
+            cpu.selectionArray[index] = input;
+        }
+    }
+        
+
+    return {addMarkToPlayer,user,cpu};
+})();
+
 const gameBoard = (function () {
     //Object properties
     const gameBoardArr = ['', '', '', '', '', '', '', '', ''];
-    let currentUser = 'X';
+    let currentPlayer = 'X';
     //cache DOM
     const grids = document.querySelectorAll('.field');
     const playerTurn = document.getElementById('player-turn');
 
     //bind EVENTS
     grids.forEach(grid => grid.addEventListener('click', (e) => {
-        addMark(grid.getAttribute('data-index'),currentUser);
+        addMark(grid.getAttribute('data-index'),currentPlayer);
         renderBoard();
         renderPlayerTurn();
     }));
@@ -21,14 +44,15 @@ const gameBoard = (function () {
     }
 
     function renderPlayerTurn(){
-        playerTurn.textContent = `Player ${currentUser} Turn`;
+        playerTurn.textContent = `Player ${currentPlayer} Turn`;
     }
 
-    function addMark(field,currentUser){
+    function addMark(field,currentPlayer){
         if(checkFieldAvailability(field)){
             console.log(`field available`);
-            gameBoardArr[field - 1] = currentUser;
-            switchCurrentUser();
+            gameBoardArr[field - 1] = currentPlayer;
+            gameController.addMarkToPlayer(currentPlayer,field);
+            switchcurrentPlayer();
         }else{
             console.log(`field not available`);
         }
@@ -50,8 +74,8 @@ const gameBoard = (function () {
         renderBoard();
     }
 
-    function switchCurrentUser(){
-        (currentUser == 'O' ? currentUser = 'X' : currentUser = 'O');
+    function switchcurrentPlayer(){
+        (currentPlayer == 'O' ? currentPlayer = 'X' : currentPlayer = 'O');
     }
 
     return {gameBoardArr};
